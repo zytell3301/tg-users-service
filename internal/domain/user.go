60a705b69a93c767361b3e325565ea287b58cf9d
Arshiya Kiani <zytell3301@gmail.com>
2021-11-05 16:56:40 +0330

    Update User type dude to changes in users table

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -6,6 +6,9 @@ import (
 
 type User struct {
 	Id            string
+	Name          string
+	Lastname      string
+	Bio           string
 	Username      string
 	Phone         string
 	Online_status bool
