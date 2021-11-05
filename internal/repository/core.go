bf098813b9f1d55aa9c2807522ef052e061ca5db
Arshiya Kiani <zytell3301@gmail.com>
2021-11-05 23:43:46 +0330

    Proceed the implementation of NewUsersRepository method

--- internal/repository/core.go
+++ internal/repository/core.go
@@ -1,14 +1,25 @@
 package repository
 
 import (
+	"github.com/gocql/gocql"
 	"github.com/zytell3301/cassandra-query-builder"
 )
 
 type Repository struct {
-	metadata cassandraQB.TableMetadata
+	metadata   cassandraQB.TableMetadata
 	connection cassandraQB.Connection
 }
 
-func NewUsersRepository() Repository {
-	return Repository{}
+func NewUsersRepository(hosts []string) (Repository, error) {
+	connection := cassandraQB.Connection{
+		Cluster: gocql.NewCluster(hosts...),
+		Session: nil,
+	}
+	session, err := connection.Cluster.CreateSession()
+	switch err != nil {
+	case true:
+		return Repository{}, err
+	}
+	connection.Session = session
+	return Repository{connection: connection}, nil
 }
