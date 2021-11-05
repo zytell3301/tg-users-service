ff8f6b95eaadcd966c865fe32ea3c5d834266214
Arshiya Kiani <zytell3301@gmail.com>
2021-11-05 15:19:47 +0330

    Add NewUser method

--- internal/core/logic.go
+++ internal/core/logic.go
@@ -1,5 +1,7 @@
 package core
 
+import "github.com/zytel3301/tg-users-service/internal/domain"
+
 type Service struct {
 	repository UsersRepository
 }
@@ -8,4 +10,14 @@ func NewUsersCore(repository UsersRepository) Service {
 	return Service{
 		repository: repository,
 	}
+}
+
+func (s Service) NewUser(user domain.User) (err error) {
+	err = s.repository.NewUser(user)
+	switch err != nil {
+	case true:
+		// @TODO once the logger service implemented, this part must report the error to logger service
+	}
+
+	return
 }
