f7d064f8d0719e63da3d10a7bd4bccfbd104886c
Arshiya Kiani <zytell3301@gmail.com>
2021-11-08 23:56:33 +0330

    Proceed NewUser method implementation

--- internal/repository/core.go
+++ internal/repository/core.go
@@ -2,7 +2,9 @@ package repository
 
 import (
 	"github.com/gocql/gocql"
+	"github.com/zytel3301/tg-users-service/internal/domain"
 	"github.com/zytell3301/cassandra-query-builder"
+	uuid_generator "github.com/zytell3301/uuid-generator"
 )
 
 type Repository struct {
@@ -45,4 +47,35 @@ func NewUsersRepository(hosts []string) (Repository, error) {
 	usersMetadata.Connection = connection.Session
 
 	return Repository{connection: connection, metadata: usersMetadata}, nil
+}
+
+func (r Repository) NewUser(user domain.User) (err error) {
+	batch := &gocql.Batch{
+		Type: gocql.LoggedBatch,
+		Cons: gocql.All,
+	}
+	data := map[string]interface{}{
+		"id":            user.Id,
+		"username":      user.Username,
+		"name":          user.Name,
+		"lastname":      user.Lastname,
+		"phone":         user.Phone,
+		"bio":           user.Bio,
+		"online_status": user.Online_status,
+		"created_at":    user.Created_at,
+	}
+	generator, _ := uuid_generator.NewGenerator("")
+	err = cassandraQB.AddId(&data, nil, generator)
+	switch err != nil {
+	case true:
+		return
+	}
+	err = r.metadata.NewRecord(data, batch)
+	switch err != nil {
+	case true:
+		return
+	}
+
+	err = r.connection.Session.ExecuteBatch(batch)
+	return
 }
