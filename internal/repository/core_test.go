355267ca885b2420d0c4ad43c79ea433e1ce3e85
Arshiya Kiani <zytell3301@gmail.com>
2021-11-10 01:24:56 +0330

    Adapt tests with new changes in NewUsersRepository method

--- internal/repository/core_test.go
+++ internal/repository/core_test.go
@@ -1,15 +1,26 @@
 package repository
 
 import (
+	"github.com/zytel3301/tg-users-service/internal/domain"
 	uuid_generator "github.com/zytell3301/uuid-generator"
 	"testing"
+	"time"
 )
 
 var hosts = []string{"192.168.1.200"}
 var idGenerator, _ = uuid_generator.NewGenerator("")
+var dummyUser = domain.User{
+	Name:          "RK",
+	Lastname:      "800",
+	Bio:           "This is a test bio",
+	Username:      "zimens40K",
+	Phone:         "+09999999999",
+	Online_status: false,
+	Created_at:    time.Now(),
+}
 
 func TestNewUsersRepository(t *testing.T) {
-	repo, err := NewUsersRepository(hosts, idGenerator)
+	repo, err := NewUsersRepository(hosts, "tg", idGenerator)
 	switch err != nil || repo.connection.Session == nil || repo.connection.Cluster == nil {
 	case true:
 		t.Errorf("An error encountered while creating a new repo. Error: %v", err)
@@ -17,7 +28,7 @@ func TestNewUsersRepository(t *testing.T) {
 }
 
 func TestNewUsersRepository2(t *testing.T) {
-	_,err := NewUsersRepository(nil,idGenerator)
+	_, err := NewUsersRepository(nil, "tg", idGenerator)
 	switch err == nil {
 	case true:
 		t.Error("Expected to return error but no error returned")
