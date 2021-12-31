df19f927728e536577dd917934e746cea4ff5873
Arshiya Kiani <zytell3301@gmail.com>
2022-01-01 02:16:08 +0330

    Add grpc handler for NewUser method

---
+++ internal/handlers/grpcHandlers/handlers.go
@@ -0,0 +1,37 @@
+package grpcHandlers
+
+import (
+	"context"
+	"errors"
+	"github.com/zytell3301/tg-users-service/internal/core"
+	"github.com/zytell3301/tg-users-service/internal/domain"
+	"github.com/zytell3301/tg-users-service/pkg/UsersService"
+	error1 "github.com/zytell3301/tg-users-service/pkg/error"
+	"time"
+)
+
+type Handler struct {
+	UsersService.UnimplementedUsersServiceServer
+	core core.Service
+}
+
+func (h Handler) NewUser(ctx context.Context, user *UsersService.User) (*error1.Error, error) {
+	err := h.core.NewUser(domain.User{
+		Name:       user.Name,
+		Lastname:   user.Lastname,
+		Phone:      user.Phone,
+		Created_at: time.Now(),
+	})
+
+	switch {
+	case errors.As(err, &core.UserAlreadyExists{}):
+		return &error1.Error{
+			Message: core.UserAlreadyExistsError.Message,
+			Code:    core.UserAlreadyExistsError.Code,
+		}, nil
+	}
+
+	return &error1.Error{
+		Code: 0,
+	}, nil
+}
