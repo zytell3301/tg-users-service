ad533e08449bf232631f563f38b3c667e6353141
Arshiya Kiani <zytell3301@gmail.com>
2022-01-01 21:41:49 +0330

    Update UpdateUser method interface

--- internal/core/repository.go
+++ internal/core/repository.go
@@ -4,6 +4,6 @@ import "github.com/zytell3301/tg-users-service/internal/domain"
 
 type UsersRepository interface {
 	NewUser(domain.User) error
-	UpdateUser(domain.User) error
+	UpdateUser(string,domain.User) error
 	DeleteUser(string) error
 }
