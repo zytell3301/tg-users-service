19b9c40ba28f762535becc1977bcf757a93ca888
Arshiya Kiani <zytell3301@gmail.com>
2022-01-07 23:37:46 +0330

    Add another test for NewUser method

--- internal/core/core_test.go
+++ internal/core/core_test.go
@@ -3,6 +3,7 @@ package core
 import (
 	"errors"
 	"github.com/golang/mock/gomock"
+	errors2 "github.com/zytell3301/tg-globals/errors"
 	"github.com/zytell3301/tg-users-service/internal/domain"
 	"testing"
 )
@@ -49,5 +50,29 @@ func TestService_NewUser2(t *testing.T) {
 	switch err == nil || !errors.As(err, &UserAlreadyExists{}) {
 	case true:
 		t.Errorf("Expected NewUser to return error but no error returned")
+	}
+}
+
+/*
+ * Test case for internal failure
+ */
+func TestService_NewUser3(t *testing.T) {
+	controller := gomock.NewController(t)
+	defer controller.Finish()
+	repositoryMock := NewMockUsersRepository(controller)
+	repositoryMock.EXPECT().NewUser(user).Return(errors.New(""))
+	repositoryMock.EXPECT().DoesUserExists(user.Phone).Return(false, nil)
+
+	core := NewUsersCore(repositoryMock)
+
+	err := core.NewUser(user)
+	switch err == nil {
+	case true:
+		t.Errorf("Expected NewUser to return error but no error returned")
+	}
+
+	switch !errors.As(err, &errors2.InternalError{}) {
+	case true:
+		t.Error("Expected NewUser to return error from type InternalError but error is from different type")
 	}
 }
