3db97f2888411a18ae9a35e3ea274b1fa2142365
Arshiya Kiani <zytell3301@gmail.com>
2022-01-10 01:42:32 +0330

    Proceed implementation of service startup

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"github.com/spf13/viper"
+	core2 "github.com/zytell3301/tg-users-service/internal/core"
+	"github.com/zytell3301/tg-users-service/internal/handlers/grpcHandlers"
+	"github.com/zytell3301/tg-users-service/internal/repository"
+	"github.com/zytell3301/tg-users-service/pkg/UsersService"
+	uuid_generator "github.com/zytell3301/uuid-generator"
+	"google.golang.org/grpc"
 	"log"
+	"net"
 )
 
 const ProjectRoot = "./.."
@@ -24,7 +31,21 @@ type repositoryConfigs struct {
 }
 
 func main() {
-
+	configs := configs{}
+	configs.repositoryConfigs = loadRepositoryConfigs()
+	configs.serviceConfigs = loadServiceConfigs()
+	uuidGenerator, err := uuid_generator.NewGenerator(configs.serviceConfigs.uuidSpace)
+	switch err != nil {
+	case true:
+		log.Fatalf("An error occurred while initiating uuid generator instance. Error message: %v", err)
+	}
+	repo, err := repository.NewUsersRepository(configs.repositoryConfigs.hosts, configs.repositoryConfigs.keyspace, uuidGenerator)
+	usersCore := core2.NewUsersCore(repo)
+	grpcHandler := grpcHandlers.NewHandler(usersCore)
+	listener, err := net.Listen("tcp", configs.serviceConfigs.nodeIp+":"+configs.serviceConfigs.servicePort)
+	grpcServer := grpc.NewServer()
+	UsersService.RegisterUsersServiceServer(grpcServer,grpcHandler)
+	grpcServer.Serve(listener)
 }
 
 func loadConfig(config string) *viper.Viper {
