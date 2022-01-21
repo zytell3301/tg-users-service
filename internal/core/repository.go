0f2e41818b27297b34947ef901257e3f949457ba
Arshiya Kiani <zytell3301@gmail.com>
2022-01-21 15:25:46 +0330

    Rename RecordSms method to a proper name

--- internal/core/repository.go
+++ internal/core/repository.go
@@ -8,5 +8,5 @@ type UsersRepository interface {
 	DeleteUser(phone string) error
 	DoesUserExists(phone string) (bool, error)
 	DoesUsernameExists(username string) (bool, error)
-	RecordSMS(code string) error
+	RecordSecurityCode(code string) error
 }
