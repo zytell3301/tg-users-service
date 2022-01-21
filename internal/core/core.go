cb8af645a2e95c4536a48cc40be786a59b8d7476
Arshiya Kiani <zytell3301@gmail.com>
2022-01-22 00:28:46 +0330

    Add hashExpression method

--- internal/core/core.go
+++ internal/core/core.go
@@ -5,6 +5,7 @@ import (
 	"github.com/zytell3301/tg-error-reporter"
 	"github.com/zytell3301/tg-globals/errors"
 	"github.com/zytell3301/tg-users-service/internal/domain"
+	"golang.org/x/crypto/bcrypt"
 	"math/big"
 	"strconv"
 )
@@ -100,7 +101,12 @@ func (s Service) DeleteUser(phone string) (err error) {
 	return
 }
 
+func hashExpression(expression string) string {
+	hashedExpression, _ := bcrypt.GenerateFromPassword([]byte(expression), 12)
+	return string(hashedExpression)
+}
+
 func generateSecurityCode() string {
-	securityCode, _ := rand.Int(rand.Reader,big.NewInt(999999-100000))
-	return strconv.Itoa(int(securityCode.Int64())+100000)
+	securityCode, _ := rand.Int(rand.Reader, big.NewInt(999999-100000))
+	return strconv.Itoa(int(securityCode.Int64()) + 100000)
 }
