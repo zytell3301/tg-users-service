cc1d7a826e8d42148eaf04a12bad7635f7e86efe
Arshiya Kiani <zytell3301@gmail.com>
2022-01-29 00:21:52 +0330

    Add NewCertificate method

--- pkg/CertGen/CertGetn.go
+++ pkg/CertGen/CertGetn.go
@@ -1,15 +1,23 @@
 package CertGen
 
 import (
+	"crypto/rand"
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"math/big"
 )
 
 type CertGen struct {
 	pool   *x509.CertPool
 	CaCert *x509.Certificate
 	CaKey  *rsa.PrivateKey
+}
+
+var max = new(big.Int)
+
+func init() {
+	max.Exp(big.NewInt(2), big.NewInt(130), nil).Sub(max, big.NewInt(1))
 }
 
 func NewCertGenerator(CaCert []byte, CaKey []byte) (CertGen, error) {
@@ -30,6 +38,16 @@ func NewCertGenerator(CaCert []byte, CaKey []byte) (CertGen, error) {
 	gen.CaCert = cert
 	gen.CaKey = privateKey
 	return gen, nil
+}
+
+func (c CertGen) NewCertificate(cert *x509.Certificate) ([]byte, error) {
+	cert.SerialNumber = GenerateUniqueId()
+	return x509.CreateCertificate(rand.Reader, cert, c.CaCert, c.CaKey.PublicKey, c.CaKey)
+}
+
+func GenerateUniqueId() *big.Int {
+	id, _ := rand.Int(rand.Reader, max)
+	return id
 }
 
 func ParseCert(ca []byte) (*x509.Certificate, error) {
