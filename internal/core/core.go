15aa28032f1c31a1de0a40ddf713f05daefc4630
Arshiya Kiani <zytell3301@gmail.com>
2022-01-29 01:04:58 +0330

    Implement reportDeleteUserError method

--- internal/core/core.go
+++ internal/core/core.go
@@ -222,6 +222,14 @@ func (s Service) reportDoesUserExistsError(err error) {
 	})
 }
 
+func (s Service) reportDeleteUserError(err error) {
+	go s.ErrorReporter.Report(ErrorReporter.Error{
+		ServiceId:  s.serviceId,
+		InstanceId: s.instanceId,
+		Message:    fmt.Sprintf("An error occurred while deleting user from database. Error message: %v", err.Error()),
+	})
+}
+
 func hashExpression(expression string) string {
 	hashedExpression, _ := bcrypt.GenerateFromPassword([]byte(expression), 12)
 	return string(hashedExpression)
