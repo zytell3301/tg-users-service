42782b2073f8912c54fc6f066f2d2cf15e178b23
Arshiya Kiani <zytell3301@gmail.com>
2022-02-21 01:34:16 +0330

    Add errorReporter package for reporting service errors

---
+++ internal/errorReporter/reporter.go
@@ -0,0 +1,36 @@
+package errorReporter
+
+import (
+	"fmt"
+	ErrorReporter "github.com/zytell3301/tg-error-reporter"
+)
+
+type reportError func(message string, parameters ...string)
+
+type reporter struct {
+	instanceId    string
+	serviceId     string
+	errorReporter ErrorReporter.Reporter
+	reportError
+}
+
+var Reporter reporter
+
+func NewReporter(instanceId string, serviceId string) reporter {
+	r := reporter{
+		instanceId: instanceId,
+		serviceId:  serviceId,
+	}
+	r.reportError = func(message string, parameters ...string) {
+		r.errorReporter.Report(ErrorReporter.Error{
+			ServiceId:  r.serviceId,
+			InstanceId: r.instanceId,
+			Message:    fmt.Sprintf(message, parameters),
+		})
+	}
+	return r
+}
+
+func ReportError(message string, parameters ...string) {
+	go Reporter.reportError(message, parameters)
+}
