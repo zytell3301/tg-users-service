1d98fe9567d465a32c081253471cac983595a2b0
Arshiya Kiani <zytell3301@gmail.com>
2022-02-28 10:48:41 +0330

    Add todo

--- internal/core/core.go
+++ internal/core/core.go
@@ -245,6 +245,7 @@ func (s Service) requestSecurityCode(phone string, action string) (err error) {
  * Verifies given security code and action.
  * If the security code is incorrect SecurityCodeNotValid error will be returned.
  * If the security code is correct but the action is incorrect, SecurityCodeActionDoesNotMatch will be returned
+ * @TODO Determine maximum attempts for security code validation
  */
 func (s Service) VerifySecurityCode(phone string, code string, action string) error {
 	securityCode, err := s.repository.GetSecurityCode(phone)
